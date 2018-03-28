dec603fc068c4e7c501bdcb79aaa88aa5ee9f1d2
beyond_champion <beyond_champion@163.com>
2018-03-28 15:55:11 +0800

    完善common-algorithm项目，优化【二维数组的查找】 算法，完善注释

--- src/com/algorithm/findnuminarray/FindNumInArray.java
+++ src/com/algorithm/findnuminarray/FindNumInArray.java
@@ -7,6 +7,9 @@ package com.algorithm.findnuminarray;
  * Time: 14:09
  * To change this template use File | Settings | File Templates.
  * Description:二维数组的查找
+ *
+ * 在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+ * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
  */
 public class FindNumInArray {
 
